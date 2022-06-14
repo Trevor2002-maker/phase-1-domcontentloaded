3ea3b1d81bfbb61db9fd6365e1067c6a939c65fa
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-14 15:40:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",() =>{
+    
+    console.log(document.querySelector('#text'));
+    const p = document.getElementById('text');
+p.textContent = 'This is really cool!'
+console.log("The DOM has loaded");
+});
